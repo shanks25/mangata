e10976f44adffcd3c14054e37e4b9c1a43aa2e43
axay <axayhg@gmail.com>
2018-08-30 19:39:56 +0530

    bambora added

--- resources/views/user/payment/partial/bambora.blade.php
+++ resources/views/user/payment/partial/bambora.blade.php
@@ -4,27 +4,27 @@
         <div class="row no-margin" id="card-payment">
             <div class="form-group col-md-12 col-sm-12">
                 <label>@lang('user.card.fullname')</label>
-                <input data-stripe="name" autocomplete="off" required type="text" class="form-control"
+                <input name="name" autocomplete="off" required type="text" class="form-control"
                        placeholder="@lang('user.card.fullname')">
             </div>
             <div class="form-group col-md-12 col-sm-12">
                 <label>@lang('user.card.card_no')</label>
-                <input data-stripe="number" type="text" onkeypress="return isNumberKey(event);" required
+                <input name="number" type="text" onkeypress="return isNumberKey(event);" required
                        autocomplete="off" maxlength="16" class="form-control" placeholder="@lang('user.card.card_no')">
             </div>
             <div class="form-group col-md-4 col-sm-12">
                 <label>@lang('user.card.month')</label>
                 <input type="text" onkeypress="return isNumberKey(event);" maxlength="2" required autocomplete="off"
-                       class="form-control" data-stripe="exp-month" placeholder="MM">
+                       class="form-control" name="exp-month" placeholder="MM">
             </div>
             <div class="form-group col-md-4 col-sm-12">
                 <label>@lang('user.card.year')</label>
                 <input type="text" onkeypress="return isNumberKey(event);" maxlength="2" required autocomplete="off"
-                       data-stripe="exp-year" class="form-control" placeholder="YY">
+                       name="exp-year" class="form-control" placeholder="YY">
             </div>
             <div class="form-group col-md-4 col-sm-12">
                 <label>@lang('user.card.cvv')</label>
-                <input type="text" data-stripe="cvc" onkeypress="return isNumberKey(event);" required autocomplete="off"
+                <input type="text" name="cvc" onkeypress="return isNumberKey(event);" required autocomplete="off"
                        maxlength="4" class="form-control" placeholder="@lang('user.card.cvv')">
             </div>
         </div>
