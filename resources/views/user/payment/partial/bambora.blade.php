947ad35ee00a63003d263f24cab242f6623abf1c
axay <axayhg@gmail.com>
2018-08-30 19:26:38 +0530

    bambora added

--- resources/views/user/payment/partial/bambora.blade.php
+++ resources/views/user/payment/partial/bambora.blade.php
@@ -34,3 +34,60 @@
         <button type="submit" class="login-btn">@lang('user.card.add_card')</button>
     </div>
 </form>
+@section('scripts')
+    <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+
+    <script type="text/javascript">
+        Stripe.setPublishableKey("{{ Setting::get('stripe_publishable_key')}}");
+
+        var stripeResponseHandler = function (status, response) {
+            var $form = $('#payment-form');
+
+            console.log(response);
+
+            if (response.error) {
+                // Show the errors on the form
+                $form.find('.payment-errors').text(response.error.message);
+                $form.find('button').prop('disabled', false);
+                alert('error');
+
+            } else {
+                // token contains id, last4, and card type
+                var token = response.id;
+                // Insert the token into the form so it gets submitted to the server
+                $form.append($('<input type="hidden" id="stripeToken" name="stripe_token" />').val(token));
+                jQuery($form.get(0)).submit();
+            }
+        };
+
+        $('#payment-form').submit(function (e) {
+
+            if ($('#stripeToken').length == 0) {
+                console.log('ok');
+                var $form = $(this);
+                $form.find('button').prop('disabled', true);
+                console.log($form);
+                Stripe.card.createToken($form, stripeResponseHandler);
+                return false;
+            }
+        });
+
+    </script>
+    <script type="text/javascript">
+        function isNumberKey(evt) {
+            var charCode = (evt.which) ? evt.which : event.keyCode;
+            if (charCode != 46 && charCode > 31
+                && (charCode < 48 || charCode > 57))
+                return false;
+
+            return true;
+        }
+
+        $('#card_pay').on('change', function () {
+
+            if ($(this).is(':checked')) {
+                $('$card_id').val($(this).val());
+            }
+        })
+    </script>
+@endsection
