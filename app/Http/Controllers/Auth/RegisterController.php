913e491a122fa55f8f506d9d0cdb1c7e06da1613
axay <axayhg@gmail.com>
2018-08-31 08:27:40 +0530

    bambora added

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -193,7 +193,7 @@ class RegisterController extends Controller
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->phone = $request->phone_number;
+        $user->phone = $request->phone;
         $user->password = bcrypt($request->password);
         $user->save();
 
