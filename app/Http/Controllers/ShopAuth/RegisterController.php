626d93c4f0a963c155f37ded2a36d0eba3c51654
axay <axayhg@gmail.com>
2018-08-31 20:49:28 +0530

    driver

--- app/Http/Controllers/ShopAuth/RegisterController.php
+++ app/Http/Controllers/ShopAuth/RegisterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\ShopAuth;
 
 use App\Shop;
-use Illuminate\Support\Facades\Validator;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -44,16 +43,14 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array $data
+     * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
     {
-
-        dd($data);
-
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:shops',
             'password' => 'required|min:6|confirmed',
         ]);
@@ -62,7 +59,7 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array $data
+     * @param  array  $data
      * @return Shop
      */
     protected function create(array $data)
@@ -71,10 +68,6 @@ class RegisterController extends Controller
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
-            'phone' => $data['phone'],
-            'address' => $data['address'],
-            'longitude' => $data['longitude'],
-            'latitude' => $data['latitude'],
             'password' => bcrypt($data['password']),
         ]);
     }
