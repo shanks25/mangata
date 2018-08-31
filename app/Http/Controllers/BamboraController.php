84f3047d0a4f9c6c1ee8b432a2a85d4fffe45db9
axay <axayhg@gmail.com>
2018-08-31 21:45:16 +0530

    driver

--- app/Http/Controllers/BamboraController.php
+++ app/Http/Controllers/BamboraController.php
@@ -30,38 +30,42 @@ class BamboraController extends Controller
 
     public function makePayment(MakePaymentRequest $request)
     {
+        try {
 
-        $user = Auth::user();
+            $user = Auth::user();
 
-        if ($request->card_id) {
-            $card = Card::where('user_id', $user->id)->where('id', $request->card_id)->first();
-        } else {
-            $card = Card::where('user_id', $user->id)->where('is_default', 1)->first();
-        }
-
-        if ($card) {
-
-            $this->payment_data = array(
-                'order_number' => 'ORD' . rand(),
-                'amount' => $request->payable,
-                'payment_method' => 'card',
-                'card' => array(
-                    'name' => $card->name,
-                    'number' => $card->number,
-                    'expiry_month' => $card->exp_month,
-                    'expiry_year' => $card->exp_year,
-                    'cvd' => $card->cvc
-                )
-            );
-
-            //Try to submit a Card Payment
-            $result = $this->beanstream->payments()->makeCardPayment($this->payment_data, $this->complete);
-
-            if ($result->approved) {
-                return $result;
+            if ($request->card_id) {
+                $card = Card::where('user_id', $user->id)->where('id', $request->card_id)->first();
+            } else {
+                $card = Card::where('user_id', $user->id)->where('is_default', 1)->first();
             }
 
+            if ($card) {
 
+                $this->payment_data = array(
+                    'order_number' => 'ORD' . rand(),
+                    'amount' => $request->payable,
+                    'payment_method' => 'card',
+                    'card' => array(
+                        'name' => $card->name,
+                        'number' => $card->number,
+                        'expiry_month' => $card->exp_month,
+                        'expiry_year' => $card->exp_year,
+                        'cvd' => $card->cvc
+                    )
+                );
+
+                //Try to submit a Card Payment
+                $result = $this->beanstream->payments()->makeCardPayment($this->payment_data, $this->complete);
+
+                if ($result->approved) {
+                    return $result;
+                }
+
+
+            }
+        } catch (\Exception $e) {
+            dd($e);
         }
 
     }
