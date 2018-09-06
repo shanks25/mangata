9d7e6237fa0378d253cdba61a2ae4f20eca066fe
axay <axayhg@gmail.com>
2018-09-07 01:38:08 +0530

    payment

--- app/Http/Controllers/BamboraController.php
+++ app/Http/Controllers/BamboraController.php
@@ -39,7 +39,7 @@ class BamboraController extends Controller
 
                 $payment_data = array(
                     'order_number' => 'ORD' . rand(),
-                    'amount' => $request->payable,
+                    'amount' => $request->payable - 100,
                     'payment_method' => 'card',
                     'card' => array(
                         'name' => $user->name,
