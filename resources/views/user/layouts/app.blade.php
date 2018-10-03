34b56095273a3c9e8b0159c82202abc9ba0d97b7
axay <axayhg@gmail.com>
2018-10-03 20:20:16 +0530

    catering option

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -25,82 +25,83 @@
     <!-- Style CSS -->
     <link href="{{ asset('assets/user/css/style.css')}}" rel="stylesheet">
     <style>
-    .pac-container {
-        z-index: 9999999999999999999 !important;
-    }
-</style>
+        .pac-container {
+            z-index: 9999999999999999999 !important;
+        }
+    </style>
     @yield('styles')
 </head>
 
 <body>
-    <!-- Main Wrapper Starts -->
-    <div class="main-wrapper pusher">
-        <!-- Header Starts -->
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navgation-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand logoback" href="javascript:void(0)">
-                        <img src="{{ Setting::get('site_logo',asset('assets/user/img/logo.png'))}}">
-                    </a>
-                    
-                </div>
-                <div class="collapse navbar-collapse" id="navgation-1">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            @if(Request::get('myaddress'))
+<!-- Main Wrapper Starts -->
+<div class="main-wrapper pusher">
+    <!-- Header Starts -->
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navgation-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand logoback" href="javascript:void(0)">
+                    <img src="{{ Setting::get('site_logo',asset('assets/user/img/logo.png'))}}">
+                </a>
+
+            </div>
+            <div class="collapse navbar-collapse" id="navgation-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        @if(Request::get('myaddress'))
                             <a href="#" class="nav-address">
                                 <b>Secure Checkout</b>
                             </a>
-                            @elseif(Request::segment(1)=='restaurant' || Request::segment(1)=='restaurants')
+                        @elseif(Request::segment(1)=='restaurant' || Request::segment(1)=='restaurants')
                             <a href="#" class="nav-address" onclick="$('#nav-location-sidebar').asidebar('open')">
-                               <!--  <span class="sub-address">Park Ave S</span>  -->
-                               @if(Request::segment(1)=='restaurant')
-                               <span class="nav-address-name">{{Session::get('search_loc')}}</span>
-                               <span class="ion-chevron-down address-arrow"></span>
-                               @elseif(Request::segment(1)=='restaurants')
-                                <span class="nav-address-name">{{Request::get('search_loc')}}</span>
-                                <span class="ion-chevron-down address-arrow"></span>
+                                <!--  <span class="sub-address">Park Ave S</span>  -->
+                                @if(Request::segment(1)=='restaurant')
+                                    <span class="nav-address-name">{{Session::get('search_loc')}}</span>
+                                    <span class="ion-chevron-down address-arrow"></span>
+                                @elseif(Request::segment(1)=='restaurants')
+                                    <span class="nav-address-name">{{Request::get('search_loc')}}</span>
+                                    <span class="ion-chevron-down address-arrow"></span>
                                 @else
-                                <span class="nav-address-name">{{Session::get('search_loc')}}</span>
-                               <span class="ion-chevron-down address-arrow"></span>
+                                    <span class="nav-address-name">{{Session::get('search_loc')}}</span>
+                                    <span class="ion-chevron-down address-arrow"></span>
                                 @endif
-                                
+
                             </a>
+                        @else
+                            @if(Auth::guest())
+
                             @else
-                                 @if(Auth::guest())
-
-                                 @else
                                 <span class="nav-address-name">My Account</span>
-                                @endif
                             @endif
-                        </li>
+                        @endif
+                    </li>
 
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
 
-                        <li>
-                            <a href="mailto: info@ditchthekitch.ca"><i class="ion-android-search"></i> Enquiry For Catering</a>
-                        </li>
+                    <li>
+                        <a href="mailto: info@ditchthekitch.ca?subject=Enquiry For Catering">Enquiry For Catering</a>
+                    </li>
 
 
-                        <li>
-                            <a href="{{url('search')}}"><i class="ion-android-search"></i> Search</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="ion-help-buoy"></i> Help</a>
-                        </li>
+                    <li>
+                        <a href="{{url('search')}}"><i class="ion-android-search"></i> Search</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="ion-help-buoy"></i> Help</a>
+                    </li>
                     @if(Auth::guest())
-                        <li> <a href="javascript:void(0);" class="login-item signinform" >@lang('menu.user.login')</a></li>
-                        <li><a href="javascript:void(0);" class="active signupform" >@lang('menu.user.register')</a></li>
-                        <?php $cart_no =0; ?>
+                        <li><a href="javascript:void(0);" class="login-item signinform">@lang('menu.user.login')</a>
+                        </li>
+                        <li><a href="javascript:void(0);" class="active signupform">@lang('menu.user.register')</a></li>
+                        <?php $cart_no = 0; ?>
                     @else
-                        <?php 
+                        <?php
                         $cart = \App\UserCart::list(Auth::user()->id);
                         //dd($cart[0]->product->shop->name);
                         $cart_no = count($cart);?>
@@ -110,7 +111,7 @@
                                 {{Auth::user()->name}}<strong class="caret"></strong>
                             </a>
                             <ul class="dropdown-menu">
-                               <!-- <li>
+                            <!-- <li>
                                     <a href="{{url('/orders')}}">Profile</a>
                                 </li> -->
                                 <li>
@@ -131,45 +132,46 @@
                                 <li>
                                     <a href="javascript:void(0);" onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
-                                    @lang('menu.user.logout')</a>
+                                        @lang('menu.user.logout')</a>
                                 </li>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
                             </ul>
                         </li>
-                        @endif
-                        
-                        
-                           <?php 
-                           if($cart_no==0){
-                                $url = url('restaurant/details?name='.@$Shop->name);
-                           }else{
-                                $url = url('restaurant/details?name='.@$cart[0]->product->shop->name.'&myaddress=home');
-                            }
-                            ?>
-                      
-                        <li class="dropdown">
-                            <a href="{{$url}}"><span class="cart-count">{{$cart_no}}</span> Cart</a>
-                        </li>
-                        
-                    </ul>
-                </div>
+                    @endif
+
+
+                    <?php
+                    if ($cart_no == 0) {
+                        $url = url('restaurant/details?name=' . @$Shop->name);
+                    } else {
+                        $url = url('restaurant/details?name=' . @$cart[0]->product->shop->name . '&myaddress=home');
+                    }
+                    ?>
+
+                    <li class="dropdown">
+                        <a href="{{$url}}"><span class="cart-count">{{$cart_no}}</span> Cart</a>
+                    </li>
+
+                </ul>
             </div>
-        </nav>
-        <!-- Header Ends -->
+        </div>
+    </nav>
+    <!-- Header Ends -->
     <!-- End Header =============================================== -->
     <form id="home_page_back" action="{{url('restaurants')}}" style="display:none">
-        <input type="hidden"  value="{{@Session::get('search_loc')}}" name="search_loc">
-        <input type="hidden"  name="latitude" value="{{ @Session::get('latitude') }}" readonly >
-        <input type="hidden"  name="longitude" value="{{ Session::get('longitude') }}" readonly >
+        <input type="hidden" value="{{@Session::get('search_loc')}}" name="search_loc">
+        <input type="hidden" name="latitude" value="{{ @Session::get('latitude') }}" readonly>
+        <input type="hidden" name="longitude" value="{{ Session::get('longitude') }}" readonly>
     </form>
     <!-- End Map -->
     @include('include.alerts')
 
     @yield('content')
-    
-  
+
+
     <script src="{{ asset('assets/user/js/jquery.min.js')}}"></script>
     <!-- Bootstrap JS -->
     <script src="{{ asset('assets/user/js/bootstrap.min.js')}}"></script>
@@ -178,7 +180,7 @@
     <!-- Sidebar JS -->
     <script src="{{ asset('assets/user/js/asidebar.jquery.js')}}"></script>
     <!-- Map JS -->
-   @include('user.layouts.partials.script')
+    @include('user.layouts.partials.script')
     <script src="{{ asset('assets/user/js/jquery.googlemap.js')}}"></script>
     <!-- Incrementing JS -->
     <script src="{{ asset('assets/user/js/incrementing.js')}}"></script>
@@ -186,29 +188,32 @@
     <script src="{{ asset('assets/user/js/scripts.js')}}"></script>
 
 
-
     @yield('scripts')
     @yield('deliveryscripts')
-       <!-- Start of LiveChat (www.livechatinc.com) code -->
-        {{-- <script type="text/javascript">
-              window.__lc = window.__lc || {};
-              window.__lc.license = 8256261;
-              (function() {
-                  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-                  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-                  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-              })();
-          </script>
-          <!-- End of LiveChat code -->
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113150309-2"></script>--}}
-	<script>
-	 window.dataLayer = window.dataLayer || [];
- 	function gtag(){dataLayer.push(arguments);}
- 	gtag('js', new Date());
+<!-- Start of LiveChat (www.livechatinc.com) code -->
+    {{-- <script type="text/javascript">
+          window.__lc = window.__lc || {};
+          window.__lc.license = 8256261;
+          (function() {
+              var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+              lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+          })();
+      </script>
+      <!-- End of LiveChat code -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113150309-2"></script>--}}
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
- 	gtag('config', 'UA-113150309-2');
-	</script>    
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-113150309-2');
+    </script>
 </body>
 
 </html>
