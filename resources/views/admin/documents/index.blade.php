ef4928d7b0b38d02d119be61f6711054d4b418a6
axay <axayhg@gmail.com>
2018-10-04 00:46:46 +0530

    data

--- resources/views/admin/documents/index.blade.php
+++ resources/views/admin/documents/index.blade.php
@@ -3,11 +3,6 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            @if(Setting::get('DEMO_MODE')==1)
-                <div class="col-md-12" style="height:50px;color:red;">
-                    ** Demo Mode : No Permission to Edit and Delete.
-                </div>
-            @endif
             <h4 class="card-title">Notice Board</h4>
             <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
             <div class="heading-elements">
@@ -34,18 +29,16 @@
                     @foreach($documents as $index => $document)
                         <?php //print"<pre>";print_r($Order); exit;?>
                         <tr>
-                            <td>{{ $Index + 1 }}</td>
+                            <td>{{ $index + 1 }}</td>
                             <td>{{ @$document->name }}</td>
                             <td>
                                 {!! @$document->type !!}
                             </td>
                             <td>{{ @$document->created_at }}</td>
                             <td>
-                                @if(Setting::get('DEMO_MODE')==0)
-                                    <a href="{{ route('admin.documents.edit', $document->id) }}"
-                                       class="table-btn btn btn-icon btn-success"><i class="fa fa-pencil-square-o"></i></a>
-                                <!-- <button  class="table-btn btn  btn-danger" onclick="return confirm('Do You want To Remove This Notice?');" form="resource-delete-{{ $Notice->id }}" >Remove</button> -->
-                                @endif
+                                <a href="{{ route('admin.documents.edit', $document->id) }}"
+                                   class="table-btn btn btn-icon btn-success"><i class="fa fa-pencil-square-o"></i></a>
+
                                 <form id="resource-delete-{{ $document->id }}"
                                       action="{{ route('admin.documents.destroy',$document->id)}}" method="POST">
                                     {{ csrf_field() }}
