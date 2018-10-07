e964d47c1efd7958e1f652d3b0c1a384d6d49106
axay <axayhg@gmail.com>
2018-10-07 16:27:45 +0530

    data

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,127 +1,62 @@
 @extends('user.layouts.app')
 
 @section('content')
- <!-- Content ================================================== -->
-    <div class="container margin_60_35">
+    <!-- Content ================================================== -->
+    <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <h3 class="nomargin_top">Some words about us</h3>
-                <p>
-                    Id pri consul aeterno petentium. Vivendo abhorreant et vim, et quot persecuti mel. Libris hendrerit ex sea. Duo legere evertitur an, pri hinc <strong>doctus definitiones</strong> an, vix id dicam putent. Ius ornatus instructior in.
-                </p>
-                <p>
-                    Id pri consul aeterno petentium. Vivendo abhorreant et vim, et quot persecuti mel. Libris hendrerit ex sea. Duo legere evertitur an, pri hinc doctus definitiones an, vix id dicam putent. Ius ornatus instructior in.
-                </p>
-                <h4>Mission</h4>
-                <p>
-                    Id pri consul aeterno petentium. Vivendo abhorreant et vim, et quot persecuti mel. <strong>Libris hendrerit</strong> ex sea. Duo legere evertitur an, pri hinc doctus definitiones an, vix id dicam putent. Ius ornatus instructior in.
-                </p>
-                <p>
-                    Id pri consul aeterno petentium. Vivendo abhorreant et vim, et quot persecuti mel. Libris hendrerit ex sea. Duo legere evertitur an, pri hinc doctus definitiones an, vix id dicam putent. Ius ornatus instructior in.
-                </p>
-                <h4>Filosofy</h4>
-                <p>
-                    Id pri consul aeterno petentium. Vivendo abhorreant et vim, et quot persecuti mel. <strong>Libris hendrerit</strong> ex sea. Duo legere evertitur an, pri hinc doctus definitiones an, vix id dicam putent. Ius ornatus instructior in.
-                </p>
-            </div>
-            <div class="col-md-7 col-md-offset-1 text-right hidden-sm hidden-xs">
-                <img src="{{asset('assets/user/img/devices.jpg')}}" alt="" class="img-responsive">
-            </div>
-        </div>
-        <!-- End row -->
-        <hr class="more_margin">
-        <div class="main_title">
-            <h2 class="nomargin_top">Quick Product Quality feautures</h2>
-            <p>
-                Cum doctus civibus efficiantur in imperdiet deterruisset.
-            </p>
-        </div>
-        <div class="row">
-            <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
-                <div class="feature">
-                    <i class="icon_building"></i>
-                    <h3><span>+ 1000</span> Grocery Shop</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-6 wow fadeIn" data-wow-delay="0.2s">
-                <div class="feature">
-                    <i class="icon_documents_alt"></i>
-                    <h3><span>+1000</span> Food Menu</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
-                    </p>
-                </div>
-            </div>
-        </div>
-        <!-- End row -->
-        <div class="row">
-            <div class="col-md-6 wow fadeIn" data-wow-delay="0.3s">
-                <div class="feature">
-                    <i class="icon_bag_alt"></i>
-                    <h3><span>Delivery</span> or Takeaway</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-6 wow fadeIn" data-wow-delay="0.4s">
-                <div class="feature">
-                    <i class="icon_mobile"></i>
-                    <h3><span>Mobile</span> support</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
-                    </p>
-                </div>
-            </div>
-        </div>
-        <!-- End row -->
-        <div class="row">
-            <div class="col-md-6 wow fadeIn" data-wow-delay="0.5s">
-                <div class="feature">
-                    <i class="icon_wallet"></i>
-                    <h3><span>Cash</span> payment</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-6 wow fadeIn" data-wow-delay="0.6s">
-                <div class="feature">
-                    <i class="icon_creditcard"></i>
-                    <h3><span>Secure card</span> payment</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex, appareat similique an usu.
-                    </p>
-                </div>
-            </div>
-        </div>
-        <!-- End row -->
-    </div>
-    <!-- End container -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6 nopadding features-intro-img">
-                <div class="features-bg">
-                    <div class="features-img">
+
+            <h1></h1>
+            <div class="content-block">
+                <h2>About Us</h2>
+                <div class="title-divider"></div>
+
+                <h1></h1>
+                <h6>DitchtheKitch brings you food delivery wherever you are Calgary. Order online or thru mobile app
+                    from our top restaurants partner and take your pick from the most popular cuisines.</h6>
+
+                <video width="500" height="340" controls>
+                    <source src="uploads/ditchkitch.mp4" type="video/mp4">
+                </video>
+
+                <h1></h1>
+                <h6>DitchtheKitch is your 24/7 source of online delivery for breakfast, lunch, snacks or dinner. We
+                    guarantee highest quality of delivery right at your doorsteps.</h6>
+
+                <h1></h1>
+                <h6>We satisfy your cravings.</h6>
+
+                <h1></h1>
+                <h1></h1>
+                <div class="content-block">
+                    <h2>How It Works</h2>
+                    <div class="title-divider"></div>
+
+
+                    <div class="row">
+                        <div class="column">
+                            <img src="uploads/location.png" alt="Location" style="width:50%; position: center;">
+                            <h6>Set Your Location</h6>
+                        </div>
+                        <div class="column">
+                            <img src="uploads/store.png" alt="Restaurant" style="width:50%; position: center;">
+                            <h6>Pick Restaurant</h6>
+                        </div>
+                        <div class="column">
+                            <img src="uploads/card.png" alt="Payment" style="width:50%; position: center;">
+                            <h6>Securely Pay</h6>
+                        </div>
+                        <div class="column">
+                            <img src="uploads/car.png" alt="Deliver" style="width:50%; position: center;">
+                            <h6>DitchtheKitch Deliver</h6>
+                        </div>
                     </div>
+
+
+                    <p></p>
                 </div>
             </div>
-            <div class="col-md-6 nopadding">
-                <div class="features-content">
-                    <h3>"Ex vero mediocrem"</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a lorem quis neque interdum consequat ut sed sem. Duis quis tempor nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                    </p>
-                    <p>
-                        Per ea erant aeque corpora, an agam tibique nec. At recusabo expetendis vim. Tractatos principes mel te, dolor solet viderer usu ad.
-                    </p>
-                </div>
-            </div>
+
         </div>
     </div>
-    <!-- End container-fluid  -->
     <!-- End Content =============================================== -->
 @endsection
