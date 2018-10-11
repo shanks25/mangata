3d13f6eea3e3b2d718a4ea36171c7711bb7fd4e5
axay <axayhg@gmail.com>
2018-10-12 03:57:09 +0530

    changes

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -85,7 +85,7 @@
                 <ul class="nav navbar-nav navbar-right">
 
                     <li>
-                        <a href="mailto: info@ditchthekitch.ca?subject=Enquiry For Catering" class="button-login">
+                        <a href="mailto: info@ditchthekitch.ca?subject=Enquiry For Catering" class="log-search-btn">
                             Enquiry For Catering</a>
                     </li>
 
