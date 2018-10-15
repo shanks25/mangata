f1411723e5a57697f10ddaf94769c7cc1eacfc57
axay <axayhg@gmail.com>
2018-10-15 18:40:44 +0530

    data

--- routes/admin.php
+++ routes/admin.php
@@ -9,7 +9,7 @@ Route::get('transporter/enable/{id}', 'AdminController@enableTransporter')->name
 Route::get('transporter/disable/{id}', 'AdminController@disableTransporter')->name('transporter.disable');
 Route::resource('transporters', 'Resource\TransporterResource');
 Route::get('transporters/{id}/document', 'Resource\DocumentResourceController@index')->name('transporters.docs.index');
-Route::get('transporters/{id}/document/{document_id}/edit', 'Resource\DocumentResourceController@edit')->name('transporters.docs.edit');
+Route::get('transporters/document/{document_id}/edit', 'Resource\DocumentResourceController@edit')->name('transporters.docs.edit');
 Route::post('transporters/{id}/document/{document_id}', 'Resource\DocumentResourceController@update')->name('transporters.docs.update');
 Route::resource('dispute-user', 'Resource\DisputeUserResource');
 Route::resource('categories', 'Resource\CategoryResource');
