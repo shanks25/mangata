14a59ca1521cd32fba7f275f25ce5253ec2a1534
axay <axayhg@gmail.com>
2018-10-15 17:45:50 +0530

    ok

--- resources/views/admin/transporters/documents/index.blade.php
+++ resources/views/admin/transporters/documents/index.blade.php
@@ -38,7 +38,7 @@
                                     <td>{{$transporterDoc['status']}}</td>
                                     <td>
 
-                                        <a href="{{ route('admin.transporters.docs.edit', $transporter->id, $transporterDoc['id']) }}"
+                                        <a href="{{ route('admin.transporters.docs.edit', $transporter->id, $transporterDoc->id) }}"
                                            class="table-btn btn btn-icon btn-success"><i
                                                     class="fa fa-pencil-square-o"></i></a>
 
