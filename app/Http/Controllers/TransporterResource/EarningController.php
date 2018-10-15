cc9c91997d9595c2e7445a2793d7e79990b4e9be
axay <axayhg@gmail.com>
2018-10-16 03:19:24 +0530

    earning

--- app/Http/Controllers/TransporterResource/EarningController.php
+++ app/Http/Controllers/TransporterResource/EarningController.php
@@ -27,16 +27,14 @@ class EarningController extends Controller
                         $q->where('orders.status', 'COMPLETED');
                     })->sum('net');
 
-                $Order_total_amount_cash = OrderInvoice::withTrashed()
-                    ->where('payment_mode', 'cash')
-                    ->with('orders')
+                $Order_total_tip = OrderInvoice::withTrashed()->with('orders')
                     ->whereHas('orders', function ($q) use ($id) {
                         $q->where('orders.shift_id', $id);
                         $q->where('orders.status', 'COMPLETED');
-                    })->sum('payable');
+                    })->orders->sum('tip');
 
                 $TransporterShift[0]->total_amount = (int)$Order_total_amount;
-                $TransporterShift[0]->total_amount_pay = (int)$Order_total_amount_cash;
+                $TransporterShift[0]->total_tip = (int)$Order_total_tip;
             }
 
             return $TransporterShift;
