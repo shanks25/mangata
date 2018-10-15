5e6c3c82c9ac63577b0e5f4b9e804fd3bd399893
axay <axayhg@gmail.com>
2018-10-15 20:47:51 +0530

    ok

--- resources/views/user/layouts/partials/script.blade.php
+++ resources/views/user/layouts/partials/script.blade.php
@@ -1107,16 +1107,18 @@
                 80.274816
             );
 
-                @if($Order != null) {
+            var lat = {!! $Order->shop->latitude !!};
 
-                var lat = {!! $Order->shop->latitude !!};
+                {{--@if($Order != null) {--}}
 
-                var lng = {!! $Order->shop->longitude !!};
+                {{----}}
 
-                var userLocation = new google.maps.LatLng(
-                    lat, lng
-                );
-            }
+                {{--var lng = {!! $Order->shop->longitude !!};--}}
+
+                {{--var userLocation = new google.maps.LatLng(--}}
+                    {{--lat, lng--}}
+                {{--);--}}
+            {{--}--}}
 
 
             map = new google.maps.Map(document.getElementById('my_map'), {
