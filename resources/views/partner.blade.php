de3914d1fe7856f8c42de10300d057c7a978e571
axay <axayhg@gmail.com>
2018-10-16 02:17:07 +0530

    ok

--- resources/views/partner.blade.php
+++ resources/views/partner.blade.php
@@ -9,19 +9,20 @@
     <div class="container">
         <div class="row">
 
-            <h3>Become a Courier</h3>
-            <h4>Earn extra income in your spare time</h4>
+            <br>
+            <h5>Become a Courier</h5>
+            <h6>Earn extra income in your spare time</h6>
             <p>Get paid weekly. Work peak days to maximize your earnings.</p>
             <br>
 
-            <h4>Flexible schedule</h4>
+            <h6>Flexible schedule</h6>
             <p>Choose hours that work for you. Take time off, or work extra when it suits you.</p>
             <br>
 
-            <h4>Know Where You're Going</h4>
+            <h6>Know Where You're Going</h6>
             <p>See earnings, pickup and drop off locations for each order before you accept the assignment.</p>
             <br>
-            <h4>Fast Support</h4>
+            <h6>Fast Support</h6>
             <p>Work with a dedicated Courier Support team, ready to chat whenever you need them.</p>
 
         </div>
