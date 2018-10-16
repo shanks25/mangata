9617da28049677a532bde4cd77890adaffdad435
axay <axayhg@gmail.com>
2018-10-16 14:44:54 +0530

    ok

--- routes/admin.php
+++ routes/admin.php
@@ -22,6 +22,7 @@ Route::resource('products', 'Resource\ProductResource');
 Route::delete('productimage/{id}', 'Resource\ProductResource@imagedestroy')->name('productimage.destroy');
 Route::resource('orders', 'Resource\OrderResource');
 Route::resource('shops', 'Resource\ShopResource');
+Route::get('shope/enquiry', 'Resource\ShopResource@enquiry');
 Route::resource('zones', 'Resource\ZoneResource');
 Route::resource('cuisines', 'Resource\CuisineResource');
 Route::resource('demoapp', 'ManageappController');
