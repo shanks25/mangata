32f051a38545ee7ce181ea29d22459f1b79bd637
axay <axayhg@gmail.com>
2018-10-16 16:25:48 +0530

    ok

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,7 +46,7 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-        $this->redirectTo = URL::previous();
+        $this->redirectTo = "/";
     }
 
     /**
