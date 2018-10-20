1954dee32e3f9b9215a3c71cd9bdebd8961e3666
axay <axayhg@gmail.com>
2018-10-20 18:31:08 +0530

    ok

--- app/Http/Controllers/UserResource/OrderResource.php
+++ app/Http/Controllers/UserResource/OrderResource.php
@@ -230,6 +230,8 @@ class OrderResource extends Controller
 
                             $baseDistance = Setting::get('base_delivery_km', 3);
 
+                            dd($totalDistance);
+
                             if ($totalDistance > $baseDistance) {
                                 $deliveryCharge = (($totalDistance - $baseDistance) * Setting::get('after_base_charges', 1)) * Setting::get('delivery_charge', 3);
                             } else {
